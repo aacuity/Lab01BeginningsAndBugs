47f43697e4a3ce5a43d689598127de1197b340ba
aacuity <noahkeiser@gmail.com>
2025-01-27 13:40:46 -0700

    Update Bugged.java

--- src/Bugged.java
+++ src/Bugged.java
@@ -6,19 +6,17 @@ public class Bugged {
      * @param num1
      * @param num2
      */
-    public static boolean printer(String val, int num1, double num2){ //4 errors
-        System.out.println(val + "is my favorite color!");
-        num3 = 42;
+    public static boolean printer(String val, int num1, double num2) { 
+        System.out.println(val + " is my favorite color!");
+        int num3 = 42;
 
-        double num4 = num1 + num2 * num3
-        System.out.println("Your number is", num3);
+        double num4 = num1 + num2 * num3;
+        System.out.println("Your number is " + num3);
 
-        System.out.println();
-        return num4 == num1 + num2 * num3
+        return num4 == num1 + num2 * num3;
     }
 
     /**
-     *
      * A broken calculator with the number 3 permanently jammed and the remainder button also permanently jammed
      * This means the first value entered into the calculator is always 3
      * This also means the symbol used is always %
@@ -26,9 +24,13 @@ public class Bugged {
      * @param val
      * @return Returns true at the end to indicate it could do the calculation
      */
-    public static void jammedCalculator(int val){ //4 errors
-        int jammedVal == 3;
-        System.out.println("Your result is " jammedVal % val)
+    public static boolean jammedCalculator(int val) { 
+        int jammedVal = 3;
+        if (val == 0) {
+            System.out.println("Error: Division by zero.");
+            return false;
+        }
+        System.out.println("Your result is " + (jammedVal % val));
         return true;
     }
 
@@ -37,9 +39,9 @@ public class Bugged {
      *
      * @param args
      */
-    public static void main(String[] args){ //4 errors
-        System.out.println(printer("Indigo", 1 4.0));
-        System.out.println(jammedCalculator("4"))
-        System.out.printLn("Done!");
+    public static void main(String[] args) { 
+        System.out.println(printer("Indigo", 1, 4.0));
+        System.out.println(jammedCalculator(4));
+        System.out.println("Done!");
     }
 }
