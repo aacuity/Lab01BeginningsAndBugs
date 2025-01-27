41f22f962da3ea5cc39549690fb9124a9ba7f855
aacuity <noahkeiser@gmail.com>
2025-01-27 13:42:17 -0700

    Update Bugged.java

--- src/Bugged.java
+++ src/Bugged.java
@@ -6,14 +6,15 @@ public class Bugged {
      * @param num1
      * @param num2
      */
-    public static boolean printer(String val, int num1, double num2) { 
+    public static boolean printer(String val, int num1, double num2) { // 4 errors fixed
         System.out.println(val + " is my favorite color!");
         int num3 = 42;
 
-        double num4 = num1 + num2 * num3;
-        System.out.println("Your number is " + num3);
+        double num4 = num1 + num2 * num3; // Fixed missing semicolon
+        System.out.println("Your number is " + num3); // Fixed incorrect println syntax
 
-        return num4 == num1 + num2 * num3;
+        System.out.println();
+        return num4 == num1 + num2 * num3; // Fixed missing semicolon
     }
 
     /**
@@ -24,14 +25,10 @@ public class Bugged {
      * @param val
      * @return Returns true at the end to indicate it could do the calculation
      */
-    public static boolean jammedCalculator(int val) { 
-        int jammedVal = 3;
-        if (val == 0) {
-            System.out.println("Error: Division by zero.");
-            return false;
-        }
-        System.out.println("Your result is " + (jammedVal % val));
-        return true;
+    public static boolean jammedCalculator(int val) { // 4 errors fixed
+        int jammedVal = 3; // Fixed incorrect variable declaration
+        System.out.println("Your result is " + (jammedVal % val)); // Fixed missing concatenation and semicolon
+        return true; // Fixed method type from void to boolean
     }
 
     /**
@@ -39,9 +36,9 @@ public class Bugged {
      *
      * @param args
      */
-    public static void main(String[] args) { 
-        System.out.println(printer("Indigo", 1, 4.0));
-        System.out.println(jammedCalculator(4));
-        System.out.println("Done!");
+    public static void main(String[] args) { // 4 errors fixed
+        System.out.println(printer("Indigo", 1, 4.0)); // Fixed missing comma
+        System.out.println(jammedCalculator(4)); // Fixed incorrect parameter type
+        System.out.println("Done!"); // Fixed incorrect println capitalization
     }
 }
