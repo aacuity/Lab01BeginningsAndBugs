8df6519b495c31bff26ad3018e10306da3a614b6
aacuity <noahkeiser@gmail.com>
2025-01-28 10:08:20 -0700

    Update Bugged.java

--- src/Bugged.java
+++ src/Bugged.java
@@ -1,4 +1,9 @@
 public class Bugged {
+    /*
+    * 
+    * This one just prints some random stuff and does some random math
+    * 
+    */
     /**
      * This one just prints some random stuff and does some random math
      *
@@ -6,18 +11,27 @@ public class Bugged {
      * @param num1
      * @param num2
      */
-    public static boolean printer(String val, int num1, double num2) { // 4 errors fixed
+    public static boolean printer(String val, int num1, double num2) { //4 errors
         System.out.println(val + " is my favorite color!");
         int num3 = 42;
 
-        double num4 = num1 + num2 * num3; // Fixed missing semicolon
-        System.out.println("Your number is " + num3); // Fixed incorrect println syntax
+        double num4 = num1 + num2 * num3;
+        System.out.println("Your number is " + num3);
 
-        System.out.println();
-        return num4 == num1 + num2 * num3; // Fixed missing semicolon
+        return num4 == num1 + num2 * num3;
     }
 
+    /*
+    *
+    * A broken calculator with the number 3 permanently jammed and the remainder button also permanently jammed
+    * This means the first value entered into the calculator is always 3
+    * This also means the symbol used is always %
+    * 
+    * Returns true at the end to indicate it could do the calculation
+    *
+    */
     /**
+     *
      * A broken calculator with the number 3 permanently jammed and the remainder button also permanently jammed
      * This means the first value entered into the calculator is always 3
      * This also means the symbol used is always %
@@ -25,20 +39,23 @@ public class Bugged {
      * @param val
      * @return Returns true at the end to indicate it could do the calculation
      */
-    public static boolean jammedCalculator(int val) { // 4 errors fixed
-        int jammedVal = 3; // Fixed incorrect variable declaration
-        System.out.println("Your result is " + (jammedVal % val)); // Fixed missing concatenation and semicolon
-        return true; // Fixed method type from void to boolean
+    public static boolean jammedCalculator(int val) { //4 errors
+        int jammedVal = 3;
+        System.out.println("Your result is " + (jammedVal % val));
+        return true;
     }
 
+    /*
+     * 
     /**
-     * This is where your program actually runs everything
+    * This is where your program actually runs everything
+     * 
      *
      * @param args
-     */
-    public static void main(String[] args) { // 4 errors fixed
-        System.out.println(printer("Indigo", 1, 4.0)); // Fixed missing comma
-        System.out.println(jammedCalculator(4)); // Fixed incorrect parameter type
-        System.out.println("Done!"); // Fixed incorrect println capitalization
+    */
+    public static void main(String[] args) { //4 errors
+        System.out.println(printer("Indigo", 1, 4.0));
+        System.out.println(jammedCalculator(4));
+        System.out.println("Done!");
     }
 }
